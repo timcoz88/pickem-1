532e3bd429d53b0e1c16ceb6752009285ba95d33
Bruno Val <bhsousa@gmail.com>
2018-03-12 17:54:03 -0300

    index main form

--- index-main.php
+++ index-main.php
@@ -86,35 +86,6 @@
 							</div>
 						</div>
 						<div class="form-bottom">
-<<<<<<< Upstream, based on origin/master
-							<form role="form" action="" method="post"
-								class="registration-form">
-								<div class="form-group">
-									<label class="sr-only" for="form-first-name">First name</label>
-									<input type="text" name="form-first-name"
-										placeholder="First name..."
-										class="form-first-name form-control" id="form-first-name">
-								</div>
-								<div class="form-group">
-									<label class="sr-only" for="form-last-name">Last name</label> <input
-										type="text" name="form-last-name" placeholder="Last name..."
-										class="form-last-name form-control" id="form-last-name">
-								</div>
-								<div class="form-group">
-									<label class="sr-only" for="form-email">Email</label> <input
-										type="text" name="form-email" placeholder="Email..."
-										class="form-email form-control" id="form-email">
-								</div>
-								<div class="form-group">
-									<label class="sr-only" for="form-password">Password</label> <input
-										type="text" name="form-password" placeholder="Password..."
-										class="form-password form-control" id="form-password">
-								</div>
-								<div class="form-group">
-									<label class="sr-only" for="form-password-confirm">Confirm
-										password</label> <input type="text"
-										name="form-password-confirm" placeholder="Confirm password"
-=======
 							<form role="form" action="user-register-post.php" method="post"
 								class="registration-form">
 								<div class="form-group">
@@ -135,14 +106,13 @@
 								</div>
 								<div class="form-group">
 									<label class="sr-only" for="form-password">Password</label> <input
-										type="password" name="senha" placeholder="Password..."
+										type="text" name="senha" placeholder="Password..."
 										class="form-password form-control" id="form-password">
 								</div>
 								<div class="form-group">
 									<label class="sr-only" for="form-password-confirm">Confirm
-										password</label> <input type="password"
+										password</label> <input type="text"
 										name="confirma-senha" placeholder="Confirm password"
->>>>>>> bd4fb1a diversos
 										class="form-password-confirm form-control"
 										id="form-password-confirm">
 								</div>
